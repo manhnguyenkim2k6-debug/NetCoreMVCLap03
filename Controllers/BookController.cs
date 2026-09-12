573e4cdb4ef4402b57c9c26e6618edcbd916ec36
ManhNguyenKim <ADMIN@LAPTOP-PD4OJTVI>
2026-09-12 22:24:15 +0700

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCLap03.Models;
+namespace NetCoreMVCLab03.Controllers
+{
+    public class BookController : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult Index(int? AuthorId, int? GenreId)
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+
+            var books = book.GetBookList();
+
+            // Lọc theo Tác giả nếu có chọn
+            if (AuthorId.HasValue && AuthorId.Value > 0)
+            {
+                books = books.Where(b => b.AuthorId == AuthorId.Value).ToList();
+            }
+
+            // Lọc theo Thể loại nếu có chọn
+            if (GenreId.HasValue && GenreId.Value > 0)
+            {
+                books = books.Where(b => b.GenreId == GenreId.Value).ToList();
+            }
+
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            Book model = new Book();
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+
+            Book model = book.GetBookById(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+        public PartialViewResult PopularBook()
+        {
+            var books = book.GetBookList();
+            return PartialView(books);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NetCoreMVCLap03.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public int TotalPage { get; set; }
+        public string Sumary { get; set; }
+        //
+        public List<Book> GetBookList()
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Chí Phèo ",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "~/images/b1.jbg",
+                    Price = 500000,
+                    Sumary = "",
+                    TotalPage = 250
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Nàng tiên cua ",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "~/images/b2.jbg",
+                    Price = 300000,
+                    Sumary = "",
+                    TotalPage = 200
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Title = " Sự tích con thạch sùng ",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "~/images/b3.jbg",
+                    Price = 400000,
+                    Sumary = "",
+                    TotalPage = 550
+                },
+            };
+            return books;
+        }
+        //
+        public Book GetBookById(int id)
+        {
+            Book book = this.GetBookList().FirstOrDefault(b => b.Id == id);
+            return book;
+        }
+        //
+        public List<SelectListItem> Authors { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Chí Phèo" },
+            new SelectListItem { Value = "2", Text = "Nàng tiên cua " },
+            new SelectListItem { Value = "3", Text = "Sự tích con thạch sùng" }
+        };
+        public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Truyện tranh" },
+            new SelectListItem { Value = "2", Text = "Truyện cổ tích" },
+            new SelectListItem { Value = "3", Text = "Truyện dân gian" }
+        };
+    }
+}
